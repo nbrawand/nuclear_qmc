86e78aad8367746456287465ec2c378f908c629a
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-04-30 10:34:25 -0400

    add ke test

--- tests/test_wave_function.py
+++ tests/test_wave_function.py
@@ -1,3 +1,4 @@
+from nuclear_qmc.constants.constants import H_BAR_SQRD_OVER_2_M
 from nuclear_qmc.wave_function.wave_function import WaveFunction
 import numpy as np
 import jax.numpy as jnp
@@ -13,8 +14,18 @@ class TestWaveFunction:
         assert True
 
     def test_kinetic_energy(self):
-        r_coords = jnp.array([[0., 1., 2.], [0., 0., 0.]])
-        computed = self.WFC.kinetic_energy(r_coords)
+        wfc = WaveFunction(1, 1)
+        wfc.spin = jnp.array([1, 0])
+        wfc.psi = lambda x: x.reshape(-1).sum() ** 2 * wfc.spin
+        r_coords = jnp.array([[1., 0., 0.], [0., 0., 0.]])
+        grad_psi = 2.0 * wfc.spin
+        n_particles = 2
+        n_dims = 3
+        ke_psi = - H_BAR_SQRD_OVER_2_M * grad_psi * n_particles * n_dims
+        psi_r = wfc.psi(r_coords)
+        expected = jnp.dot(psi_r, ke_psi)
+        computed = wfc.kinetic_energy(r_coords)
+        assert expected == computed
 
     def test_tau_sigma(self):
         wfc = jnp.array([[0., 1., 2., 3], [4., 5, 6, 7], [8, 9, 10, 11]])  # 4 spin states, 3 isospin states
