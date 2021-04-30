68477eb9f71c4c225a88e9a353ab4ab66ffcfd66
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-04-30 09:36:39 -0400

    add sigma tests

--- tests/test_wave_function.py
+++ tests/test_wave_function.py
@@ -17,11 +17,21 @@ class TestWaveFunction:
         computed = self.WFC.kinetic_energy(r_coords)
 
     def test_tau_sigma(self):
-        wfc = np.array([[0., 1., 2., 3], [4., 5, 6, 7], [8, 9, 10, 11]])  # 4 spin states, 3 isospin states
-        xi = np.array([[0, 2, 1, 3], [3, 1, 2, 0]]).T  # 2 pair exchanges
-        pair_coefs = np.array([0, 1])
-        expected = np.array([[6., 1., 2., -3.],
-                             [10., 5., 6., 1.],
-                             [14., 9., 10., 5.]])
+        wfc = jnp.array([[0., 1., 2., 3], [4., 5, 6, 7], [8, 9, 10, 11]])  # 4 spin states, 3 isospin states
+        xi = jnp.array([[0, 2, 1, 3], [3, 1, 2, 0]]).T  # 2 pair exchanges
+        pair_coefs = jnp.array([0, 1])
+        expected = jnp.array([[6., 1., 2., -3.],
+                              [10., 5., 6., 1.],
+                              [14., 9., 10., 5.]])
+        computed = self.WFC._tau_or_sigma(wfc, xi, pair_coefs)
+        assert jnp.array_equal(expected, computed)
+
+    def test_tau_sigma_single_exchange(self):
+        wfc = jnp.array([[0., 1., 2., 3], [4., 5, 6, 7], [8, 9, 10, 11]])  # 4 spin states, 3 isospin states
+        xi = jnp.array([[0, 2, 1, 3]]).T  # 1 pair exchange
+        pair_coefs = 1
+        expected = jnp.array([[0., 3., 0., 3.],
+                              [4., 7., 4., 7.],
+                              [8., 11., 8., 11.]])
         computed = self.WFC._tau_or_sigma(wfc, xi, pair_coefs)
         assert jnp.array_equal(expected, computed)
