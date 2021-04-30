073094b1e14926cae8d8b2bde700dc505a51bc11
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-04-30 11:27:55 -0400

    add simple NN example wfc

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from jax.config import config
-from jax import random
-# from nuclear_qmc.wave_function.wave_function_base import WaveFunctionBase as WaveFunction
-from nuclear_qmc.hamiltonian.hamiltonian import one_particle_kinetic_energy
-from nuclear_qmc.wave_function.wave_function_single_orbital import WaveFunctionSingleOrbital as WaveFunction
+from jax import random, vmap
+from nuclear_qmc.hamiltonian.hamiltonian import get_local_energy
+# from nuclear_qmc.wave_function.wave_function_single_orbital import WaveFunctionSingleOrbital as WaveFunction
+# from nuclear_qmc.wave_function.wave_function import WaveFunction as WaveFunction
+from nuclear_qmc.wave_function.nn_two_h import TwoBodyNeuralNetwork as WaveFunction
 from nuclear_qmc.sample import sample
 
 config.update("jax_enable_x64", True)
@@ -13,15 +14,17 @@ N_NEUTRON = 1
 SEED = 0
 INITIAL_WALKER_STANDARD_DEVIATION = 1.0
 WALKER_STEP_SIZE = 1.0
-N_WALKERS = 200
+N_WALKERS = 2000
 N_DIMENSIONS = 3
-N_EQUILIBRIUM_STEPS = 20
-N_STEPS = 10
-N_VOID_STEPS = 10
-wave_function = WaveFunction(N_PROTON, N_NEUTRON)
-
+N_EQUILIBRIUM_STEPS = 100
+N_STEPS = 20
+N_VOID_STEPS = 50
+# wave_function = WaveFunction(N_PROTON, N_NEUTRON)
 key = random.PRNGKey(SEED)
-key, r_coords = sample(
+wave_function = WaveFunction(N_DIMENSIONS, N_PROTON + N_NEUTRON, 1, key, 0.0, N_PROTON, N_NEUTRON,
+                             params_file='pionless_4_nucleus_2.model')
+
+key, r_coord_samples = sample(
     wave_function
     , N_STEPS
     , INITIAL_WALKER_STANDARD_DEVIATION
@@ -34,3 +37,7 @@ key, r_coords = sample(
     , key
 )
 
+r_coord_samples = r_coord_samples.reshape(-1, N_PROTON + N_NEUTRON, N_DIMENSIONS)
+local_energy = vmap(get_local_energy, in_axes=(None, 0))(wave_function, r_coord_samples)
+
+print(local_energy.mean())
---
+++ nuclear_qmc/wave_function/nn_two_h.py
@@ -0,0 +1,90 @@
+import jax
+import jax.numpy as jnp
+from jax import random, jit, vmap
+from jax.flatten_util import ravel_pytree
+from jax.experimental import stax
+from jax.experimental.stax import Dense, elementwise, Tanh
+from jax.nn.initializers import glorot_normal, normal
+from functools import partial
+import pickle
+
+from nuclear_qmc.wave_function.wave_function import WaveFunction
+
+
+@jit
+def lintanh(x):
+    return x - jnp.tanh(x) / 2
+
+
+Lintanh = elementwise(lintanh)
+Sin = elementwise(jnp.sin)
+
+
+class TwoBodyNeuralNetwork(WaveFunction):
+    def __init__(self, ndim, npart, conf, key, mix, n_protons, n_neutrons, params_file=None):
+        super().__init__(n_protons, n_neutrons)
+        self.ndim = ndim
+        self.npart = npart
+        self.conf = conf
+        self.key = key
+        self.mix = mix
+        self.ndense = 8
+        self.nlat = 32  ## 1 * (self.ndim * self.npart + 4)
+        self.activation = Tanh
+        self.a = 8
+        self._build()
+        self.params_file = params_file
+        if self.params_file is not None:
+            self.params = self.load_params(self.params_file)
+
+    def _build(self):
+        # phi_a
+        self.phi_a_init, self.phi_a_apply = stax.serial(
+            Dense(self.ndense), self.activation,
+            Dense(self.ndense), Tanh,
+            Dense(1),
+        )
+        in_shape = (1,)
+        self.key, key_input = jax.random.split(self.key)
+        phi_a_shape, phi_a_params = self.phi_a_init(key_input, in_shape)
+        self.num_phi_a_params = len(phi_a_params)
+        self.params = phi_a_params
+
+    def save(self, file_name):
+        with open(file_name, 'wb') as file:
+            pickle.dump(self.params, file)
+
+    def load_params(self, params_file_name):
+        with open(params_file_name, 'rb') as fil:
+            return pickle.load(fil)
+
+    @partial(jit, static_argnums=(0,))
+    def psi(self, r):
+        rcm = jnp.mean(r, axis=0)
+        r = r - rcm[None, :]
+        delta_r = jnp.linalg.norm(r[0, :] - r[1, :])
+
+        phi_a_params = self.params[0:self.num_phi_a_params]
+        phi_a_out = self.phi_a_apply(phi_a_params, delta_r)
+        phi_a_out = jnp.mean(phi_a_out)
+
+        psi = jnp.exp(phi_a_out)
+        psi *= self.phi(r)
+        return jnp.reshape(psi, ()) * self.spin
+
+    @partial(jit, static_argnums=(0,))
+    def phi(self, r):
+        """ Boundary condition imposed on multiple particles
+        """
+        rcm = jnp.mean(r, axis=0)
+        r = r - rcm[None, :]
+        return jnp.prod(vmap(self.sp_boundary, in_axes=(0))(r))
+
+    @partial(jit, static_argnums=(0,))
+    def sp_boundary(self, r):
+        """ Boundary condition imposed on single particle 
+        """
+        sp_conf = jnp.exp(- self.conf * jnp.sum(r ** 2))
+
+        return sp_conf
+
