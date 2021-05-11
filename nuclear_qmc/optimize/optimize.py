d375e1931dacaadc2896d428a4ead1849dbf6e9a
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-05-11 15:39:25 -0400

    add optimize test

--- nuclear_qmc/optimize/optimize.py
+++ nuclear_qmc/optimize/optimize.py
@@ -1,15 +1,14 @@
 import jax
 from jax import vmap, numpy as jnp
 from jax.scipy.linalg import cho_factor, cho_solve
-from nuclear_qmc.operators.hamiltonian import hamiltonian_psi, get_local_energy
-from nuclear_qmc.operators.operators import kinetic_energy_psi
+from nuclear_qmc.operators.hamiltonian import hamiltonian_psi
 
 
 def d_psi_d_params(psi, psi_params, r_coord):
     return jax.grad(psi, argnums=0)(psi_params, r_coord)
 
 
-def get_new_wave_function_parameters(
+def get_delta_params(
         psi
         , psi_params
         , psi_vector
@@ -17,7 +16,10 @@ def get_new_wave_function_parameters(
         , particle_pairs
         , particle_triplets
         , spin_exchange_indices
-        , learning_rate):
+        , learning_rate
+        , hamiltonian=hamiltonian_psi
+        , include_sr_equations=True
+        , return_loss=False):
     """
 
     Parameters
@@ -34,7 +36,7 @@ def get_new_wave_function_parameters(
 
     d_psi = vmap(d_psi_d_params, in_axes=(None, None, 0))(psi, psi_params, r_coords)
     d_psi = jnp.tensordot(d_psi, psi_vector, axes=0)  # walkers, params, spin-isospin
-    h_psi = vmap(hamiltonian_psi, in_axes=(None, None, None, 0, None, None, None))(
+    h_psi = vmap(hamiltonian, in_axes=(None, None, None, 0, None, None, None))(
         psi, psi_params, psi_vector, r_coords, particle_pairs, particle_triplets, spin_exchange_indices)
     psi_r = vmap(psi, in_axes=(None, 0))(psi_params, r_coords)
     psi_r = jnp.tensordot(psi_r, psi_vector, axes=0)
@@ -44,24 +46,30 @@ def get_new_wave_function_parameters(
     d_psi_psi = vmap(lambda x, y: vmap(jnp.vdot, in_axes=(0, 0))(x, y), in_axes=(1, None))(d_psi, psi_r)
     psi_h_psi = vmap(jnp.vdot, in_axes=(0, 0))(psi_r, h_psi)
     d_psi_psi_avg = (d_psi_psi / psi_psi).mean(axis=1)
-    d_energy = 2.0 * (d_psi_h_psi / psi_psi).mean(axis=1) - 2.0 * (psi_h_psi / psi_psi).mean(axis=0) * d_psi_psi_avg
-    # return - learning_rate * d_energy
+    loss = (psi_h_psi / psi_psi).mean(axis=0)
+    d_energy = 2.0 * (d_psi_h_psi / psi_psi).mean(axis=1) - 2.0 * loss * d_psi_psi_avg
+    delta_p = - learning_rate * d_energy
 
-    d_psi_d_psi = \
-        vmap(
+    if include_sr_equations:
+        d_psi_d_psi = \
             vmap(
                 vmap(
-                    jnp.vdot
-                    , in_axes=(None, 0))
-                , in_axes=(0, None))
-            , in_axes=(0, 0))(d_psi, d_psi)  # walkers, params, spin-isospin
-    d_psi_d_psi_avg = (d_psi_d_psi / jnp.expand_dims(psi_psi, axis=(1, 2))).mean(axis=0)
-    psi_d_psi_avg = (jnp.conj(d_psi_psi) / psi_psi).mean(axis=1)
-    d_psi_psi_avg_psi_d_psi_avg = jnp.tensordot(d_psi_psi_avg, psi_d_psi_avg, axes=0)
-    S_ij = d_psi_d_psi_avg - d_psi_psi_avg_psi_d_psi_avg
+                    vmap(
+                        jnp.vdot
+                        , in_axes=(None, 0))
+                    , in_axes=(0, None))
+                , in_axes=(0, 0))(d_psi, d_psi)  # walkers, params, spin-isospin
+        d_psi_d_psi_avg = (d_psi_d_psi / jnp.expand_dims(psi_psi, axis=(1, 2))).mean(axis=0)
+        psi_d_psi_avg = (jnp.conj(d_psi_psi) / psi_psi).mean(axis=1)
+        d_psi_psi_avg_psi_d_psi_avg = jnp.tensordot(d_psi_psi_avg, psi_d_psi_avg, axes=0)
+        S_ij = d_psi_d_psi_avg - d_psi_psi_avg_psi_d_psi_avg
 
-    eps = 0.0001
-    S_ij += eps * jnp.identity(S_ij.shape[0])
-    cho_factor_solution = cho_factor(S_ij)
-    delta_p = cho_solve(cho_factor_solution, -learning_rate * d_energy)
-    return delta_p
+        eps = 0.0001
+        S_ij += eps * jnp.identity(S_ij.shape[0])
+        cho_factor_solution = cho_factor(S_ij)
+        delta_p = cho_solve(cho_factor_solution, -learning_rate * d_energy)
+
+    if return_loss:
+        return delta_p, loss
+    else:
+        return delta_p
--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -1,24 +1,44 @@
-# from nuclear_qmc.optimize.optimize import get_new_wave_function_parameters, partial_psi_prefactor_parameters, \
-#     partial_full_psi_parameters
-# from nuclear_qmc.operators.operators import kinetic_energy_psi
-# import jax.numpy as jnp
+from nuclear_qmc.optimize.optimize import get_delta_params
+import jax.numpy as jnp
 
 
 class TestOptimize:
     def test_get_new_wave_function_parameters(self):
-        """
-        wfc = WaveFunction(1, 1)
-        wfc.params = jnp.array([1., 1., 1.])
-        wfc.psi_prefactor = lambda x, p: x[0, 0] * p[0]
-        # wfc.psi_vector = lambda x, p, s: jnp.array([[1., 0.], [0., 2.]])
-        n_walkers = 10
-        r_coords = n_walkers * [[[1., 0., 0.], [0., 0., 0.]]]
-        r_coords = jnp.array(r_coords)
-        learning_rate = 0.001
-        computed = get_new_wave_function_parameters(wfc
-                                                    , r_coords
-                                                    , learning_rate
-                                                    , partial_function=partial_full_psi_parameters
-                                                    , kinetic_energy_operator=kinetic_energy_psi)
-                                                    """
-        assert True
+        """A loss function that is the diff of psi and a parabola is optimized for several steps.
+        The parameter should match the parabola center"""
+        offset = abs(1.0)
+        psi = lambda p, x: (x - p[0]) ** 2 + offset  # the offset is needed to avoid zero.
+        target_parameter_value = 1.5
+        psi_params = jnp.array([0.8])
+        psi_vector = jnp.array([1., 1.])
+        r_coords = jnp.arange(-10, 10.01, 0.01)
+        particle_pairs = None
+        particle_triplets = None
+        spin_exchange_indices = None
+        learning_rate = 0.1
+
+        def hamiltonian(psi
+                        , psi_params
+                        , psi_vector
+                        , r_coords
+                        , particle_pairs
+                        , particle_triplets
+                        , spin_exchange_indices):
+            return -((r_coords - target_parameter_value) ** 2 - psi(psi_params, r_coords)) ** 2 * psi_vector
+
+        for n in range(20):
+            delta_params, loss = get_delta_params(
+                psi
+                , psi_params
+                , psi_vector
+                , r_coords
+                , particle_pairs
+                , particle_triplets
+                , spin_exchange_indices
+                , learning_rate
+                , hamiltonian=hamiltonian
+                , include_sr_equations=False
+                , return_loss=True)
+            psi_params += delta_params
+        computed = psi_params.round(1)
+        assert target_parameter_value == computed + offset
