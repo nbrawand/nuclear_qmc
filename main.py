a7c20951eeec9b7a60ac53d3e85b816627378e1d
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-05-13 13:08:46 -0400

    update main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from jax import random, vmap
 from nuclear_qmc.operators.hamiltonian import get_local_energy
 from nuclear_qmc.optimize.optimize import get_delta_params
 from nuclear_qmc.sampling.sample import sample
-from nuclear_qmc.wave_function.jastro_neural_network import build_jastro_wave_function_no_spin_correlations
+from nuclear_qmc.wave_function.jastro_neural_network import build_jastro_wave_function_with_spin_correlations
 from nuclear_qmc.wave_function.wave_function import get_wave_function_system
 
 config.update("jax_enable_x64", True)
@@ -16,21 +16,22 @@ N_NEUTRON = 2
 SEED = 0
 INITIAL_WALKER_STANDARD_DEVIATION = 1.0
 WALKER_STEP_SIZE = 0.2
-N_WALKERS = 1000
+N_WALKERS = 4000
 N_DIMENSIONS = 3
 N_EQUILIBRIUM_STEPS = 20
-N_STEPS = 5
+N_STEPS = 10
 N_VOID_STEPS = 200
 N_OPTIMIZATION_STEPS = 20000
 key = random.PRNGKey(SEED)
 particle_pairs, particle_triplets, spin, spin_exchange_indices, isospin_exchange_indices = get_wave_function_system(
     N_PROTON, N_NEUTRON)
 param_file = '3H_jastro_nn_wfc.model'
-ndense = 12
+ndense = 8
 psi_vector = spin
-psi_prefactor, psi_params = build_jastro_wave_function_no_spin_correlations(ndense
-                                                                   , particle_pairs
-                                                                   )
+psi_prefactor, psi_params = build_jastro_wave_function_with_spin_correlations(ndense
+                                                                              , particle_pairs
+                                                                              , spin
+                                                                              , spin_exchange_indices)
 # psi_params = jnp.load(param_file + '.npy')
 
 learning_rate = 0.0001
