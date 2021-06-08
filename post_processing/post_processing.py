bbb4dcdef183ba72211760ee7a9c22cfa2e1f41e
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-06-08 14:16:47 -0400

    update postprocessing

--- post_processing/post_processing.py
+++ post_processing/post_processing.py
@@ -10,6 +10,7 @@ def exp_model(a, b, c, opt_step):
 
 
 def fit_exp_model(energy_values, final_energy_estimate_name, sample_size):
+    trace = None
     with pm.Model() as model:
         """
         energy ~ Normal(mu, sigma)
@@ -19,18 +20,21 @@ def fit_exp_model(energy_values, final_energy_estimate_name, sample_size):
         c ~ Gaussian
         """
         a = pm.HalfCauchy("a", beta=10)
-        b = pm.HalfCauchy("b", beta=10)
-        c = pm.Normal(final_energy_estimate_name, mu=np.mean(energy_values[-10:]), sigma=10)
+        b = pm.HalfCauchy("b", beta=1)
+        c = pm.Uniform(final_energy_estimate_name, lower=-10.0, upper=0.0)
         # sa = pm.HalfCauchy("sa", beta=0.5)
         # sb = pm.HalfCauchy("sb", beta=10)
         opt_step = np.arange(len(energy_values)) + 1
-        mu = exp_model(a, b, c, opt_step)
-        sigma = pm.HalfCauchy('sigma', beta=4, shape=len(energy_values))  # np.exp(-sa * opt_step) + sb
+        beta = pm.Normal('beta', -0.5, 1)
+        mu = exp_model(a, b, c, opt_step)  # + opt_step * beta
+        sigma = pm.HalfCauchy("sigma", beta=10)  # np.exp(-sa * opt_step) + sb
         likelihood = pm.Normal("energy", mu=mu, sigma=sigma, observed=energy_values)
 
-        #trace = pm.sample(sample_size)  # draw 3000 posterior samples using NUTS sampling
+        if sample_size > 0:
+            step = pm.NUTS(adapt_step_size=True, target_accept=0.9)
+            trace = pm.sample(sample_size, step=step, tune=20000)  # draw 3000 posterior samples using NUTS sampling
     map_estimate = pm.find_MAP(model=model)
-    return 1, map_estimate
+    return trace, map_estimate
 
 
 def get_energy_values(lines):
@@ -52,17 +56,23 @@ def get_optimization_plot(markdown_file, start, samples, title=None):
     samples = int(samples)
     if samples > 0:
         energy_estimate_name = 'energy_estimate'
-        _, map = fit_exp_model(energies, energy_estimate_name, sample_size=samples)
+        trace, map = fit_exp_model(energies, energy_estimate_name, sample_size=samples)
         x = np.arange(len(energies)) + 1
         exp = exp_model(map['a'], map['b'], map[energy_estimate_name], x)
         plt.plot(exp, 'r-', lw=3, zorder=1)
-        plt.hlines(map[energy_estimate_name], 0, len(energies), colors='r', linestyles='--', lw=3, zorder=1, label=f'MAP Estimate: {np.round(map[energy_estimate_name], 4)} MeV')
+        plt.hlines(map[energy_estimate_name], 0, len(energies), colors='r', linestyles='--', lw=3, zorder=1,
+                   label=f'MAP Estimate: {np.round(map[energy_estimate_name], 4)} MeV')
         plt.legend()
 
     if title is not None:
         plt.title(title)
 
     plt.savefig('energy')
+
+    if samples > 0:
+        plt.clf()
+        az.plot_posterior(trace, var_names=energy_estimate_name)
+        plt.savefig('posterior')
 
 
 def get_results(function_name, func_args):
