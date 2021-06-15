ffc4f0508b60be633eef0f4a6da7b1e1655d0e51
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-06-15 12:45:10 -0400

    jit angular orbitals

--- nuclear_qmc/wave_function/build_angular_momentum_wave_function.py
+++ nuclear_qmc/wave_function/build_angular_momentum_wave_function.py
@@ -1,15 +1,7 @@
-from jax import vmap
+from jax import vmap, jit
 from jax.ops import index, index_update
 import jax.numpy as jnp
 from nuclear_qmc.spin.spherical_harmonics import get_spherical_harmonic_functions, get_spherical_harmonic_systems
-
-
-def get_spherical_harmonics_vector(spherical_harmonics, r_coords, particle_indices, function_permutations):
-    y_r_matrix = jnp.array([[f(r) for f in spherical_harmonics] for r in r_coords],
-                           dtype=jnp.float64)  # [n_particles, n_func]
-    y_r_vector = y_r_matrix[particle_indices, function_permutations]  # [n_permutations, n_functions]
-    y_r_vector = vmap(jnp.prod)(y_r_vector)
-    return y_r_vector
 
 
 def build_angular_momentum_wave_function(n_particles
@@ -29,12 +21,19 @@ def build_angular_momentum_wave_function(n_particles
                                                                             , L_2)
     particle_indices = jnp.arange(n_particles)
 
+    @jit
+    def get_y_r_vector(r_coords):
+        out = []
+        for spherical_harmonics in spherical_harmonics_list:
+            y_r_matrix = jnp.array([[f(r) for f in spherical_harmonics] for r in r_coords],
+                                   dtype=jnp.float64)  # [n_particles, n_func]
+            y_r_vector = y_r_matrix[particle_indices, function_permutations]  # [n_permutations, n_functions]
+            y_r_vector = vmap(jnp.prod)(y_r_vector)
+            out.append(y_r_vector)
+        return jnp.array(out, dtype=jnp.float64)
+
     def wave_function(parameters, r_coords):
-        y_r_vector = jnp.array([get_spherical_harmonics_vector(spherical_harmonics
-                                                               , r_coords
-                                                               , particle_indices
-                                                               , function_permutations)
-                                for spherical_harmonics in spherical_harmonics_list])
+        y_r_vector = get_y_r_vector(r_coords)
         y_r_vector = coefficients * y_r_vector
         y_r_vector = y_r_vector.sum(axis=0)
         new_values = y_r_vector * spin_isospin_wave_function[iso_indices, spin_indices]
