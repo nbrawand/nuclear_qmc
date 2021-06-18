b75ba5ea7e8847ec3d87c81952e74932ec918747
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-06-18 16:24:38 -0400

    add use finite diff option in hamiltonian function

--- nuclear_qmc/operators/hamiltonian/build_hamiltonian.py
+++ nuclear_qmc/operators/hamiltonian/build_hamiltonian.py
@@ -1,10 +1,13 @@
 from nuclear_qmc.operators.hamiltonian.arxiv_2007_14282v2.potential_energy import build_arxiv_2007_14282v2
 from nuclear_qmc.operators.hamiltonian.arxiv_2102_02327v1.potential_energy import build_arxiv_2102_02327v1
 from nuclear_qmc.operators.operators import kinetic_energy_psi
+from nuclear_qmc.utils.finite_difference import kinetic_energy
+import jax.numpy as jnp
 
 
 def build_hamiltonian(spin, potential_energy_expression, particle_pairs, particle_triplets, spin_exchange_indices,
-                      isospin_exchange_indices, isospin_binary_representation=None, potential_kwargs=None):
+                      isospin_exchange_indices, isospin_binary_representation=None, potential_kwargs=None
+                      , use_finite_diff=True):
     if potential_kwargs is None:
         potential_kwargs = {}
 
@@ -26,7 +29,14 @@ def build_hamiltonian(spin, potential_energy_expression, particle_pairs, particl
     potential_energy = builder(*args, **potential_kwargs)
 
     def hamiltonian(psi, psi_params, psi_vector, r_coords):
-        ke_psi = kinetic_energy_psi(psi, psi_params, r_coords) * psi_vector
+        if use_finite_diff:
+            i = jnp.arange(r_coords.shape[0])
+            j = jnp.arange(r_coords.shape[1])
+            my_psi = lambda r: psi(psi_params, r)
+            ke_psi = kinetic_energy(my_psi, r_coords, i, j) * psi_vector
+        else:
+            ke_psi = kinetic_energy_psi(psi, psi_params, r_coords) * psi_vector
+
         v_psi = potential_energy(psi, psi_params, psi_vector, r_coords)
         h_psi = ke_psi + v_psi
         return h_psi
