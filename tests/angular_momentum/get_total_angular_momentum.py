b1b3b573b67320ef1847b7049ed738c0aac9b4c4
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-06-29 09:10:35 -0400

    add angular momentum checker

---
+++ tests/angular_momentum/get_total_angular_momentum.py
@@ -0,0 +1,51 @@
+from jax import grad, jacfwd, vmap
+import jax.numpy as jnp
+import numpy as np
+from nuclear_qmc.constants.constants import H_BAR
+from jax.ops import index_update, index
+
+
+def get_rotate_r(r_coords, theta, ith_particle):
+    cos = jnp.cos(theta)
+    sin = jnp.sin(theta)
+    rotation_matrix = jnp.array([
+        [cos, -sin, 0.],
+        [sin, cos, 0.],
+        [0., 0., 1.]
+    ])
+    rotated_ith_coords_prime = jnp.einsum('ji,i', rotation_matrix, r_coords[ith_particle])
+    r_coords = index_update(r_coords, index[ith_particle], rotated_ith_coords_prime)
+    return r_coords
+
+
+def rotate_z(func_3d, r_coords, theta, ith_particle):
+    r_coords_prime = get_rotate_r(r_coords, theta, ith_particle)
+    func_out_prime = func_3d(r_coords_prime)
+    return func_out_prime
+
+
+def get_expected_value(func_3d, r_coords, L_z):
+    func_out = func_3d(r_coords)
+    total_angular_momentum = jnp.vdot(func_out, L_z) / jnp.vdot(func_out, func_out)
+    return total_angular_momentum
+
+
+def finite_diff_theta(func, r_coords, ith_particle):
+    dtheta = 0.0001
+    rot_func_r = rotate_z(func, r_coords, dtheta, ith_particle) - rotate_z(func, r_coords, -dtheta, ith_particle)
+    rot_func_r /= 2.0 * dtheta
+    return rot_func_r
+
+
+def get_total_angular_momentum_z(func, r_coords):
+    particles = jnp.arange(r_coords.shape[0])
+    partials = vmap(finite_diff_theta, in_axes=(None, None, 0))(func, r_coords, particles)
+    expected_values = vmap(get_expected_value, in_axes=(None, None, 0))(func, r_coords, partials)
+    return expected_values
+
+    # rotation_angles_rads = vmap(lambda r: jnp.arctan2(r[1], r[0]))(r_coords)
+    # particles = jnp.arange(r_coords.shape[0])
+    # rotation_func = lambda func, r, angle, particle: jacfwd(rotate_z, argnums=(2,))(func, r, angle, particle)[0]
+    # L_z = vmap(rotation_func, in_axes=(None, None, 0, 0))(func_3d, r_coords, rotation_angles_rads, particles)
+    # total_angular_momentum = vmap(get_expected_value, in_axes=(None, None, 0))(func_3d, r_coords, L_z)
+    # return total_angular_momentum
---
+++ tests/angular_momentum/test_angular_momentum.py
@@ -0,0 +1,173 @@
+from nuclear_qmc.wave_function.build_wave_function import build_wave_function
+from tests.angular_momentum.get_total_angular_momentum import finite_diff_theta
+
+from nuclear_qmc.sampling.sample import sample
+from nuclear_qmc.constants.constants import H_BAR
+import jax.numpy as jnp
+import numpy as np
+from tests.angular_momentum.get_total_angular_momentum import get_total_angular_momentum_z, get_rotate_r, rotate_z
+import jax
+
+from jax import vmap, jacfwd
+from nuclear_qmc.wave_function.spherical_harmonics import Y11, Y10, Y1m1
+
+
+def get_theta(r):
+    theta = jnp.arctan2(r[1], r[0])
+    return theta
+
+
+def get_phi(r):
+    r_mag = jnp.linalg.norm(r)
+    phi = jnp.arccos(r[2] / r_mag)
+    return phi
+
+
+def get_polar_coords(x_y_z):
+    r = jnp.linalg.norm(x_y_z)
+    theta = get_theta(x_y_z)
+    phi = get_phi(x_y_z)
+    return jnp.array([r, theta, phi])
+
+
+def get_cartesian_coords(r_theta_phi):
+    r_mag, theta, phi = r_theta_phi
+    x = r_mag * jnp.cos(theta) * jnp.sin(phi)
+    y = r_mag * jnp.sin(theta) * jnp.sin(phi)
+    z = r_mag * jnp.cos(phi)
+    return jnp.array([x, y, z])
+
+
+def convert_input_to_spherical(func):
+    def wrapper(r_theta_phi_coords):
+        r_coords = vmap(get_cartesian_coords)(r_theta_phi_coords)
+        return func(r_coords)
+
+    return wrapper
+
+
+def test_finite_diff_theta():
+    def cos(r_coords):
+        theta = get_theta(r_coords[0])
+        return jnp.cos(theta)
+
+    # check cos value
+    r_coords = jnp.array([[0., 1., 1.]])
+    computed = cos(r_coords)
+    expected = 0.0
+    assert computed.round(7) == expected
+
+    # check cos value
+    r_coords = jnp.array([[1., 0., 1.]])
+    computed = cos(r_coords)
+    expected = 1.0
+    assert computed.round(7) == expected
+
+    # test finite diff partial theta
+    r_coords = jnp.array([[1., 0., 1.]])
+    computed = finite_diff_theta(cos, r_coords, 0)
+    expected = 0.0
+    assert computed.round(4) == expected
+    r_coords = jnp.array([[0., 1., 1.]])
+    computed = finite_diff_theta(cos, r_coords, 0)
+    expected = -1.0
+    assert computed.round(3) == expected
+    r_coords = jnp.array([[1., -1., 1.]])
+    computed = finite_diff_theta(cos, r_coords, 0)
+    expected = 0.708
+    assert computed.round(3) == expected
+
+
+def test_angular_momentum_yl0():
+    r_coords = jnp.array([[1.0, 1.0, 8.324]])
+    func = lambda r: Y10(r[0])
+    z = finite_diff_theta(func, r_coords, 0)
+    assert z == 0.0
+
+
+def test_angular_momentum_ylm1():
+    r_coords = jnp.array([[1.0, 1.0, 8.324]])
+    func = lambda r: Y1m1(r[0])
+    z = finite_diff_theta(func, r_coords, 0)
+    sq34pi = jnp.sqrt(3. / (4. * jnp.pi))
+    phi = get_phi(r_coords[0])
+    theta = get_theta(r_coords[0])
+    assert z.round(3) == (sq34pi * jnp.cos(theta) * jnp.sin(phi)).round(3)
+
+
+def test_angular_momentum_yl1():
+    r_coords = jnp.array([[1.0, 1.0, 8.324]])
+    func = lambda r: Y11(r[0])
+    z = finite_diff_theta(func, r_coords, 0)
+    sq34pi = jnp.sqrt(3. / (4. * jnp.pi))
+    phi = get_phi(r_coords[0])
+    theta = get_theta(r_coords[0])
+    assert z.round(3) == (-sq34pi * jnp.sin(theta) * jnp.sin(phi)).round(3)
+
+
+def test_get_angular_momentum_he():
+    n_proton = 2
+    n_neutron = 2
+    key = jax.random.PRNGKey(0)
+    key, orbital_psi, orbital_psi_params = build_wave_function(key
+                                                               , n_neutron
+                                                               , n_proton
+                                                               , 1
+                                                               , 1)
+    func = lambda r: orbital_psi(orbital_psi_params, r)
+    r_coords = jnp.array(np.random.random(size=(n_proton + n_neutron, 3)))
+    computed = get_total_angular_momentum_z(func, r_coords)
+    expected = jnp.zeros(shape=n_proton + n_neutron)
+    assert jnp.array_equal(computed, expected)
+
+
+def test_get_angular_momentum_li():
+    n_proton = 3
+    n_neutron = 3
+    key = jax.random.PRNGKey(0)
+    key, orbital_psi, orbital_psi_params = build_wave_function(key
+                                                               , n_neutron
+                                                               , n_proton
+                                                               , 1
+                                                               , 1)
+    func = lambda r: orbital_psi(orbital_psi_params, r)
+    r_coords = jnp.array(np.random.random(size=(n_proton + n_neutron, 3)))
+    computed = get_total_angular_momentum_z(func, r_coords)
+    expected = jnp.zeros(shape=n_proton + n_neutron)
+    assert jnp.array_equal(computed, expected)
+
+# def test_get_angular_momentum_Li():
+#    # build wfc
+#    n_proton = 3
+#    n_neutron = 3
+#    key = jax.random.PRNGKey(0)
+#    key, orbital_psi, orbital_psi_params = build_wave_function(key
+#                                                               , n_neutron
+#                                                               , n_proton
+#                                                               , 1
+#                                                               , 1)
+#    psi_vector = 1.0
+#    key = jax.random.PRNGKey(0)
+#
+#    orbital_psi = lambda p, r: jnp.exp(-jnp.linalg.norm(r[0])) * Y10(r[0])
+#    key, samples = sample(orbital_psi
+#                          , orbital_psi_params
+#                          , psi_vector
+#                          , n_steps=2
+#                          , walker_step_size=0.2
+#                          , n_walkers=2000
+#                          , n_particles=n_neutron + n_proton
+#                          , n_dimensions=3
+#                          , n_equilibrium_steps=2
+#                          , n_void_steps=200
+#                          , key=key
+#                          , initial_walker_standard_deviation=1.0
+#                          )
+#    samples = samples.reshape(-1, n_neutron + n_proton, 3)
+#    func_3d = lambda r: orbital_psi(orbital_psi_params, r)
+#    rotation_angle_rads = 0.0 * jnp.pi
+#    computed = vmap(get_total_angular_momentum_z, in_axes=(None, 0, None))(func_3d, samples, rotation_angle_rads)
+#    computed /= -1.j * H_BAR
+#    print(computed.mean())
+#    expected = 0.0
+#    # assert computed == expected
