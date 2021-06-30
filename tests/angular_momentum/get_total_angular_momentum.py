f1a2e7efdd75c0132b233ce43ab0160dc8aac16f
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-06-30 10:28:59 -0400

    clean angular mom using autodiff

--- tests/angular_momentum/get_total_angular_momentum.py
+++ tests/angular_momentum/get_total_angular_momentum.py
@@ -1,7 +1,5 @@
-from jax import grad, jacfwd, vmap
+from jax import vmap, hessian
 import jax.numpy as jnp
-import numpy as np
-from nuclear_qmc.constants.constants import H_BAR
 from jax.ops import index_update, index
 
 
@@ -12,11 +10,9 @@ def get_rotate_r(r_coords, theta, ith_particle, axis):
         [[1., 0., 0.],
          [0., cos, -sin],
          [0., sin, cos]],  # x
-
         [[cos, 0., sin],
          [0., 1., 0.],
          [-sin, 0., cos]],  # y
-
         [[cos, -sin, 0.],
          [sin, cos, 0.],
          [0., 0., 1.]]  # z
@@ -32,52 +28,18 @@ def rotate(func_3d, r_coords, theta, ith_particle, axis):
     return func_out_prime
 
 
-def get_expected_value(func_3d, r_coords, L_z):
-    func_out = func_3d(r_coords)
-    total_angular_momentum = jnp.vdot(func_out, L_z) / jnp.vdot(func_out, func_out)
-    return total_angular_momentum
+def hessian_theta(func, r_coords, ith_particle, axis):
+    return hessian(rotate, argnums=(2,))(func, r_coords, 0.0, ith_particle, axis)[0][0]
 
 
-def finite_diff_theta(func, r_coords, ith_particle, axis):
-    dtheta = 0.0001
-    rot_func_r = rotate(func, r_coords, dtheta, ith_particle, axis) - rotate(func, r_coords, -dtheta, ith_particle,
-                                                                             axis)
-    rot_func_r /= 2. * dtheta
-    return rot_func_r
+def get_expected_value(psi, r_coords, o_psi):
+    psi_r = psi(r_coords)
+    return jnp.vdot(psi_r, o_psi) / jnp.vdot(psi_r, psi_r)
 
 
-def finite_diff_theta_2nd_order(func, r_coords, ith_particle, axis):
-    dtheta = 0.0001
-    rot_func_r = rotate(func, r_coords, dtheta, ith_particle, axis)
-    rot_func_r += rotate(func, r_coords, -dtheta, ith_particle, axis)
-    rot_func_r -= 2 * func(r_coords)
-    rot_func_r /= dtheta ** 2
-    return rot_func_r
-
-
-def get_L_sqrd(func, r_coords, ith_particle):
+def get_L_sqrd(psi, r_coords, ith_particle):
     axis = jnp.arange(3)
-    diffs = vmap(finite_diff_theta_2nd_order, in_axes=(None, None, None, 0))(func, r_coords, ith_particle, axis)
-    L_sqrd = diffs.sum(axis=0)
+    L_sqrd_psi = -1.0 * vmap(hessian_theta, in_axes=(None, None, None, 0))(psi, r_coords, ith_particle, axis)
+    L_sqrd = vmap(get_expected_value, in_axes=(None, None, 0))(psi, r_coords, L_sqrd_psi)
+    L_sqrd = L_sqrd.sum(axis=0)
     return L_sqrd
-
-
-def get_total_angular_momentum(func, r_coords):
-    particles = jnp.arange(r_coords.shape[0])
-    partials = vmap(get_L_sqrd, in_axes=(None, None, 0))(func, r_coords, particles)
-    expected_values = vmap(get_expected_value, in_axes=(None, None, 0))(func, r_coords, partials)
-    return expected_values
-
-
-def get_total_angular_momentum_z(func, r_coords):
-    particles = jnp.arange(r_coords.shape[0])
-    partials = vmap(finite_diff_theta, in_axes=(None, None, 0, None))(func, r_coords, particles, 2)
-    expected_values = vmap(get_expected_value, in_axes=(None, None, 0))(func, r_coords, partials)
-    return expected_values
-
-    # rotation_angles_rads = vmap(lambda r: jnp.arctan2(r[1], r[0]))(r_coords)
-    # particles = jnp.arange(r_coords.shape[0])
-    # rotation_func = lambda func, r, angle, particle: jacfwd(rotate_z, argnums=(2,))(func, r, angle, particle)[0]
-    # L_z = vmap(rotation_func, in_axes=(None, None, 0, 0))(func_3d, r_coords, rotation_angles_rads, particles)
-    # total_angular_momentum = vmap(get_expected_value, in_axes=(None, None, 0))(func_3d, r_coords, L_z)
-    # return total_angular_momentum
