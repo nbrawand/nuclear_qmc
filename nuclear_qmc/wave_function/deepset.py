7359ca0f0bb8a0474a5d6ec3320235d3f06ea51a
Nicholas Brawand <nicholasbrawand@gmail.com>
2021-07-20 13:46:05 -0400

    update deepset file

--- nuclear_qmc/wave_function/deepset.py
+++ nuclear_qmc/wave_function/deepset.py
@@ -4,17 +4,18 @@ import jax.numpy as jnp
 import jax
 
 
-def get_deep_set(key, n_dense, n_hidden_layers, in_shape, out_shape):
+def get_deep_set(key, n_dense, n_hidden_layers, in_shape, latent_shape, out_shape, wrapper_func=jnp.tanh):
     key, nn1, params1 = build_nn_wave_function(ndense=n_dense, key=key, n_hidden_layers=n_hidden_layers,
-                                               in_shape=in_shape, output_size=out_shape)
+                                               in_shape=in_shape, output_size=latent_shape)
     n_p = len(params1)
     key, nn2, params2 = build_nn_wave_function(ndense=n_dense, key=key, n_hidden_layers=n_hidden_layers,
-                                               in_shape=(out_shape,), output_size=1)
+                                               in_shape=(latent_shape,), output_size=out_shape)
 
     def psi(params, r):
         latent = jax.vmap(nn1, in_axes=(None, 0))(params[:n_p], r)
         latent = latent.sum(axis=0)
         out = nn2(params[n_p:], latent)
+        out = wrapper_func(out)
         return out
 
     params = jnp.concatenate((params1, params2))
